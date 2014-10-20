fb3b441d90aceeacd5153aff3f42c7d71a83daf0
André Freitas <p.andrefreitas@gmail.com>
2014-10-21 00:18:25 +0100

    A criar o post do customer nas routes.

--- server/app/models/show.js
+++ server/app/models/show.js
@@ -1,10 +1,7 @@
-// app/models/show.js
-
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ShowSchema   = new Schema({
-	//id: automaticamente atribuído pelo mongodb?!
 	name: String,
 	date: Date,
 	ticketPrice: Double
--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/meoarena');
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,10 +22,36 @@ var port = process.env.PORT || 8080; 		// set our port
 // =============================================================================
 var router = express.Router(); 				// get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log("Something is happening");
+	next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
+	res.json({ message: 'hooray! weleeeecome to our api!' });
 });
+
+router.route('/customers')
+
+	// Create a customer
+	.post(function(req, res){
+		var customer = new Customer();
+		customer.name = req.body.name;
+		customer.email = req.body.email;
+		customer.password = req.body.password;
+		customer.pin =
+
+		customer.save(function(err){
+			if(err)
+				res.send(err);
+
+
+		});
+
+	});
 
 // more routes for our API will happen here
 
